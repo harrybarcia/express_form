d7e9d8b33c45a7c91f7d00a394c3ec344a7077dd
harry barcia <barcia.harry@gmail.com>
2022-04-12 16:46:34 -0700

    minor changes

--- routes/Location.js
+++ routes/Location.js
@@ -3,11 +3,6 @@ const LocationController = require("../controllers/LocationController");
 const router=express.Router();
 
 
-
-// router.get("/", (req, res)=>{
-//     res.send("User:Harry")
-// })
-
 router.post("/", LocationController.post)
 
 module.exports=router;
