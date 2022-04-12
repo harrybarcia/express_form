21fbf8b0ef47710b1c9ec2a60a54417155e47189
harry barcia <barcia.harry@gmail.com>
2022-04-12 13:59:19 -0700

    old version2

--- server.js
+++ server.js
@@ -5,14 +5,9 @@ const bodyParser=require('body-parser');
 // middleware
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', (req, res)=>{
-    res.render("index.ejs")
-})
-
 const userRoute=require('./routes/User');
 app.use('/user', userRoute);
 
-
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+app.listen(3005, ()=>{
+    console.log("Server running on port 3005");
 });
