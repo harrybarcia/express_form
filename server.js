967f1f355478f16af68ab0d2ddb8fe1499b1f9ec
harry barcia <barcia.harry@gmail.com>
2022-04-12 13:59:44 -0700

    new version

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express=require("express");
 const app=express();
-const bodyParser=require('body-parser');
 
-// middleware
-app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res)=>{
+    res.render("index.ejs")
+})
 
 const userRoute=require('./routes/User');
 app.use('/user', userRoute);
 
-app.listen(3005, ()=>{
-    console.log("Server running on port 3005");
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000");
 });
